0d59a7d74ca9f03efc99f41b3385e6fc5a7229a5
VanessaTrajano <vanessapalmeirakelmertrajano@gmail.com>
2022-06-17 21:05:23 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import About from './components/Main/About'
+import LatestRecipes from './components/Main/LatestRecipes'
+import Subscribe from './components/Main/Subscribe'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+  return(
+    <>
+      <Header/>
+      <LatestRecipes/>
+      <About/>
+      <Subscribe/>
+      <Footer/>
+    </>
+  )
 }
-
-export default App;
